b64e7a2f6761207a59041d5db5075f61e1be46c7
Sebastian Valencia <sebastian.valencian@gmail.com>
2018-02-05 20:31:54 -0500

    Novena app en Angular - Firechat AngularFire2 - configuracion de la aplicacion, angularFire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyACL0IYfzV2mRhYdjrREcJMYZXexZsRcaA",
+    authDomain: "firechat-d24b8.firebaseapp.com",
+    databaseURL: "https://firechat-d24b8.firebaseio.com",
+    projectId: "firechat-d24b8",
+    storageBucket: "firechat-d24b8.appspot.com",
+    messagingSenderId: "308118614775"
+  }
 };
